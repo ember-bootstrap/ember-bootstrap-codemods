741667cdf153da17c7fcc296eeb55744a63bef3e
simonihmig <simon.ihmig@kaliber5.de>
2020-05-29 16:24:45 +0200

    Add button transform + tests

--- bin/cli.js
+++ bin/cli.js
@@ -4,5 +4,7 @@
 require('codemod-cli').runTransform(
   __dirname,
   process.argv[2] /* transform name */,
-  process.argv.slice(3) /* paths or globs */
+  process.argv.slice(3) /* paths or globs */,
+  undefined,
+  'template'
 );
--- transforms/deprecated-attribute-arguments/index.js
+++ transforms/deprecated-attribute-arguments/index.js
@@ -1,3 +1,35 @@
-module.exports = function transformer(env) {
+const deprecations = {
+  BsButton: ['title', 'disabled'],
+};
 
+function visitor(env) {
+  const b = env.syntax.builders;
+
+  return {
+    ElementNode(node) {
+      const deprecatedArguments = deprecations[node.tag];
+
+      if (deprecatedArguments) {
+        node.attributes = node.attributes.map((attrNode) => {
+          if (attrNode.name.charAt(0) !== '@') {
+            return attrNode;
+          }
+
+          let attribute = attrNode.name.substring(1);
+
+          if (!deprecatedArguments.includes(attribute)) {
+            return attrNode;
+          }
+
+          return b.attr(attribute, attrNode.value);
+        });
+      }
+    },
+  };
+}
+
+module.exports = function ({ source /*, path*/ }, { parse, visit }) {
+  const ast = parse(source);
+
+  return visit(ast, visitor);
 };
